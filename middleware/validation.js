1abc3a447237303c7f302bab108f98100a459d99
Pablo Chiquero <chiqueropablo@gmail.com>
2023-07-31 12:52:00 +0200

    comments

--- middleware/validation.js
+++ middleware/validation.js
@@ -1,4 +1,5 @@
 function validation(req,res,next) {
+// buscar en google como puedo verificar que mi variable es de tipo number y añadirlo en el if
     if(req.params.lastName = Number) {
         return "Error";
     } else {
