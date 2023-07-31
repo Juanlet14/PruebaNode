af67a0f6f2e3e256cd066d18f1f501f0ca0fa83f
juan antonio <juanleiva1493@gmail.com>
2023-07-31 11:40:10 +0200

    inital

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ const port = 3000;
 const  searchLastName  = require("./data/alumns");
 const validation = require("./middleware/validation");
 
-app.get("/:lastName",validation, (req, res) => {
+app.get("/:lastName", (req, res) => {
     // recive datos del postman
     console.log(req.params.lastName);
 
     // aqui ahora tienes que usar la funcion searchLastName() pasarle el apellido y la respuesta devolverla en el res.send()
     
     searchLastName(req.params.lastName);
+
+    
 
 
     
