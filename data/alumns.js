01d037eb97ec3c636dbbb80679e9c871e1cefe05
juan antonio <juanleiva1493@gmail.com>
2023-08-01 14:48:26 +0200

    adfas

--- data/alumns.js
+++ data/alumns.js
@@ -24,19 +24,20 @@ const alumnosArr = [
 
     },
 ]
+const searchLastName = alumnosArr.filter(alumno => alumno.lastName === "gonzalez")
 
-function searchLastName(lastName) {
+/*function searchLastName(lastName) {
     let alumno = alumnosArr.find(function (surname) {
         return surname.lastName === lastName;
 
-    });
+    });*/
     // buscar en google como hacer un busqueda dentro de un array (alumnosArr) por un parametro en concreto
     
     // SI HAS BUSCADO ESTO, BUSCA AHORA COMO HACER UN FILTER EN LA URL QUE TE PONGO AQUI ABAJO:
     // https://masteringjs.io/tutorials/fundamentals/filter-array-of-objects
-    console.log(alumno);
+    //console.log(alumnos);
 
-}
+
 
 
 module.exports = searchLastName;
